a559142011d6816c8130fee0d5d64dda8ed1deda
itsqal <faiqalanaking@gmail.com>
2025-06-02 10:46:31 +0700

    Add create report tracking template

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -35,4 +35,36 @@ class TrackingController extends Controller
     {
         return view('tracking.finish-success', compact('truck'));
     }
+
+    public function createReport(Truck $truck)
+    {        
+        if ($truck->current_status === 'tidak dalam pengiriman'){
+            return redirect(route('tracking.start-tracking', ['truck' => $truck->id]));
+        }
+
+        return view('tracking.create-report', compact('truck'));
+    }
+
+    public function storeReport(Truck $truck)
+    {
+        $validated = request()->validate([
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+            'problem_type' => 'required|string',
+            'problem_description' => 'string|nullable',
+        ], [
+            'latitude' => 'Gagal mendapatkan lokasi. Pastikan GPS aktif dan izin lokasi diberikan.',
+            'longitude' => 'Gagal mendapatkan lokasi. Pastikan GPS aktif dan izin lokasi diberikan.',
+            'problem_type.required' => 'Mohon pilih tipe kendala.'
+        ]);
+
+        // ReportProcessingJob::dispatch($truck, $validated);
+
+        return redirect()->route('tracking.report-success', ['truck' => $truck->id]);
+    }
+
+    public function reportSuccess(Truck $truck)
+    {
+        return view('tracking.report-success', compact('truck'));
+    }   
 }
